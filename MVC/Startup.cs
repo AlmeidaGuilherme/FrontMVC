a0f5f97291372a6b67d18b8c3a0b3df16338c5d0
Guilherme Almeida <guireis_almeida@hotmail.com>
2020-02-24 22:33:14 -0300

    Ajustes, adicionando banco de dados

--- MVC/Startup.cs
+++ MVC/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using MVC.Data;
 
 namespace MVC
 {
@@ -33,6 +35,10 @@ namespace MVC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<MVCContext>(options =>
+             options.UseMySql(Configuration.GetConnectionString("MVCContext"), builder =>
+            builder.MigrationsAssembly("MVC")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
